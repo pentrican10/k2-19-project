10b61e9b830e1f2e876869004891aab8402dcb5e
pentrican10 <paige.eee1014@gmail.com>
2024-06-22 13:53:11 -0700

    example ttvfast working

--- ttvfast_test.py
+++ ttvfast_test.py
@@ -2,7 +2,7 @@ import ttvfast
 from ttvfast import models
 import numpy as np
 import matplotlib.pyplot as plt
-
+'''
 # Define the parameters for the planets using models.Planet
 planet1 = models.Planet(
     mass=(32.4 * 3.0027e-6),  # M_sun
@@ -31,7 +31,8 @@ star_mass = 1.0
 time = 2000.0  # Start time in days
 dt = 0.1       # Time step in days
 total = 3500.0  # Total integration time in days
-
+N_step = (total-time) / dt
+print(N_step)
 # Pack the planetary parameters into a list
 planets = [planet1, planet2]
 
@@ -46,45 +47,6 @@ rsky_values = np.array(results['positions'][3])
 vsky_values = np.array(results['positions'][4])
 
 print(times_)
-assert 1==0
-# Separate transit times for each planet
-actual_transit_times_1 = times_[planet_ind == 0]
-epoch_1 = epoch_int[planet_ind == 0]
-actual_transit_times_2 = times_[planet_ind == 1]
-epoch_2 = epoch_int[planet_ind == 1]
-# Debug: Check the lengths of actual transit times
-print(f"actual_transit_times_1 length: {len(actual_transit_times_1)}")
-print(f"actual_transit_times_2 length: {len(actual_transit_times_2)}")
-
-# Calculate expected transit times assuming a constant orbital period
-num_transits_1 = len(actual_transit_times_1)
-num_transits_2 = len(actual_transit_times_2)
-expected_transit_times_1 = np.zeros(len(epoch_1))
-expected_transit_times_2 = np.zeros(len(epoch_2))
-for i in range(len(epoch_1)):
-    expected_transit_times = epoch_1[i] + planet1.period * i
-    expected_transit_times_1[i] = expected_transit_times
-for i in range(len(epoch_2)):
-    expected_transit_times = epoch_2[i] + planet2.period * i
-    expected_transit_times_2[i] = expected_transit_times
-
-# Calculate TTVs (actual - expected)
-ttvs_1 = actual_transit_times_1 - expected_transit_times_1
-ttvs_2 = actual_transit_times_2 - expected_transit_times_2
-
-# Convert TTVs from days to minutes for better readability
-ttvs_1_minutes = ttvs_1 * 24 * 60
-ttvs_2_minutes = ttvs_2 * 24 * 60
-
-# Plot the TTVs
-plt.figure(figsize=(10, 6))
-plt.plot(actual_transit_times_1, ttvs_1_minutes, 'o-', label='Planet 1')
-plt.plot(actual_transit_times_2, ttvs_2_minutes, 'o-', label='Planet 2')
-plt.xlabel('Time (days)')
-plt.ylabel('TTV (minutes)')
-plt.title('Transit Timing Variations')
-plt.legend()
-plt.show()
 
 '''
 from ttvfast import models
@@ -92,37 +54,103 @@ import ttvfast
 import matplotlib.pyplot as plt
 
 
+
 gravity = 0.000295994511                        # AU^3/day^2/M_sun
-stellar_mass = 0.88                    # M_sun
+
+'''
+stellar_mass = 0.95573417954                    # M_sun
 
 
 planet1 = models.Planet(
-    mass=(32.4 * 3.0027e-6),                         # M_sun
-    period=7.9222,              # days
+    mass=0.00002878248,                         # M_sun
+    period=1.0917340278625494e+01,              # days
     eccentricity=5.6159310042858110e-02,
-    inclination=91.5,         # degrees
+    inclination=9.0921164935951211e+01,         # degrees
     longnode=-1.1729336712101943e-18,           # degrees
     argument=1.8094838714599581e+02,            # degrees
     mean_anomaly=-8.7093652691581923e+01,       # degrees
 )
 
 planet2 = models.Planet(
-    mass=(10.8 * 3.0027e-6),
-    period=11.8993,
+    mass=0.00061895914,
+    period=2.2266898036209028e+01,
     eccentricity=5.6691301931178648e-02,
-    inclination=91.1,
+    inclination=8.7598285693573246e+01,
     longnode=4.6220554014026838e-01,
+    argument=1.6437004273382669e+00,
+    mean_anomaly=-1.9584857031843157e+01,
+)
+'''
+
+stellar_mass = 0.88                    # M_sun
+
+
+planet1 = models.Planet(
+    mass=(0.0777 * stellar_mass),               # M_sun
+    period=7.9222,                              # days
+    eccentricity=0.20,
+    inclination=91.5,                           # degrees
+    longnode= 0.,                   #fixed      # degrees
+    argument=1.8094838714599581e+02,            # degrees
+    mean_anomaly=-8.7093652691581923e+01,       # degrees
+)
+
+planet2 = models.Planet(
+    mass=(0.0458 * stellar_mass),
+    period=11.8993,
+    eccentricity=0.21,
+    inclination=91.1,
+    longnode=-7.4, 
     argument=1.6437004273382669e+00,
     mean_anomaly=-1.9584857031843157e+01,
 )
 
 planets = [planet1, planet2]
-Time = -1045                                    # days
-dt = 0.54                                       # days
-Total = 1700                                    # days
+Time = 2000     #-1045                        # days
+dt = 0.1                                      # days
+Total = 5500     #1700                        # days
+N_step = int((Total-Time) / dt)
+print(N_step)
 
 results = ttvfast.ttvfast(planets, stellar_mass, Time, dt, Total)
-print(results)
-plt.plot(results['positions'])
+
+# Extract necessary data from the results
+planet_ind = np.array(results['positions'][0])
+epoch_int = np.array(results['positions'][1])   #transit number
+times_ = (np.array(results['positions'][2]))
+rsky_values = np.array(results['positions'][3])
+vsky_values = np.array(results['positions'][4])
+print((times_[planet_ind==0]))
+### finding index where the time values end and -2. fills rest of array
+time_end = np.where(times_ == -2.)[0][0]
+print(time_end)
+print(results['positions'][2])
+
+# ### Reverse the array
+# reversed_array = planet_ind[::-1]
+
+# ### Find the first occurrence of 1 in the reversed array
+# first_occurrence_reversed =  np.where(reversed_array == 1)[0][0]
+
+# ### Calculate the index in the original array
+# original_index = len(planet_ind) - 1 - first_occurrence_reversed
+# print(original_index)
+
+planet_ind = planet_ind[:time_end]
+times_= times_[:time_end]
+print((results['positions'][0])[:time_end])
+
+t_1 = times_[planet_ind==0]
+t_1_aligned = t_1[1:]  # align with the np.diff calculation because it shortened the array (disregard first time)
+ttv1 = np.diff(t_1) - np.mean(np.diff(t_1))
+plt.scatter(t_1_aligned,ttv1, label='planet 1',color='orange')
+
+t_2 = times_[planet_ind==1]
+t_2_aligned = t_2[1:]
+ttv2 = np.diff(t_2) - np.mean(np.diff(t_2))
+plt.scatter(t_2_aligned, ttv2, label='planet 2', color='blue')
+plt.ylabel('TTV (days)')
+plt.xlabel('Time (days)')
+plt.title('TTV plots')
+plt.legend()
 plt.show()
-'''
